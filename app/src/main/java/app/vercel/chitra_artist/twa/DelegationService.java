e6a9ec92099dc35b6d014cfbd44132ccceedb9fb
satvik pandey <satvikpandey@satviks-MacBook-Pro.local>
2025-11-08 02:38:56 +0530

    application making

---
+++ app/src/main/java/app/vercel/chitra_artist/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.chitra_artist.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
